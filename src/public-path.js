88a3df8a34008acd79e55dbb5a46a306b5669f06
musistudio <musiteam@musiiot.top>
2020-09-16 13:35:52 +0800

    init

---
+++ src/public-path.js
@@ -0,0 +1,11 @@
+/*
+ * @Author: jinhui.li
+ * @Date: 2020-09-15 17:31:05
+ * @LastEditors: jinhui.li
+ * @LastEditTime: 2020-09-15 17:31:07
+ * @Description: 
+ */
+if (window.__POWERED_BY_QIANKUN__) {
+  // eslint-disable-next-line no-undef
+  __webpack_public_path__ = window.__INJECTED_PUBLIC_PATH_BY_QIANKUN__;
+}
---
+++ src/router/index.js
@@ -0,0 +1,31 @@
+/*
+ * @Author: jinhui.li
+ * @Date: 2020-09-15 16:52:25
+ * @LastEditors: jinhui.li
+ * @LastEditTime: 2020-09-15 17:14:22
+ * @Description: 
+ */
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Home from "../views/Home.vue";
+
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: "/",
+    name: "Home",
+    component: Home
+  },
+  {
+    path: "/about",
+    name: "About",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/About.vue")
+  }
+];
+
+export default routes;
